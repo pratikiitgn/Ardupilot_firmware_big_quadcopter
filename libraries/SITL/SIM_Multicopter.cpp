92e52d97eed6c28fd39bc6513eebbe73bed0e7ae
Randy Mackay <rmackay9@yahoo.com>
2024-07-12 15:18:16 +0900

    SITL: integrate SlungPayload

--- libraries/SITL/SIM_Multicopter.cpp
+++ libraries/SITL/SIM_Multicopter.cpp
@@ -48,6 +48,9 @@ void MultiCopter::calculate_forces(const struct sitl_input &input, Vector3f &rot
 
     add_shove_forces(rot_accel, body_accel);
     add_twist_forces(rot_accel);
+
+    // add forces from slung payload
+    add_slungpayload_forces(body_accel);
 }
     
 /*
